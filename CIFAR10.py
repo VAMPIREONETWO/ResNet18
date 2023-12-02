cdc6bb82c6f802d0d6ffd929c6d92f23e0579780
VAMOT <tianhao.duan@ucdconnect.ie>
2023-12-03 01:55:01 +0800

    ResNet

--- CIFAR10.py
+++ CIFAR10.py
@@ -1,4 +1,4 @@
-
+import tensorflow as tf
 from tensorflow import keras
 from keras import layers, Sequential, Model
 from ResNet18 import ResNet18
@@ -6,26 +6,35 @@ from keras.datasets import cifar10
 import numpy as np
 import pandas as pd
 
+using_gpu_index = 0  # 使用的 GPU 号码
+gpu_list = tf.config.experimental.list_physical_devices('GPU')
+if len(gpu_list) > 0:
+    try:
+        tf.config.experimental.set_virtual_device_configuration(
+            gpu_list[using_gpu_index],
+            [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=2048)]
+        )
+    except RuntimeError as e:
+        print(e)
+else:
+    print("Got no GPUs")
 
-# (x_train, y_train), (x_test, y_test) = cifar10.load_data()
-# print(x_train.shape)
-# print(y_train.shape)
-# print(x_train)
-# x_train = np.array(x_train)
-# print(type(x_train))
-# s = np.shape(x_train)
-# print(s)
-
-# model = Sequential()
-# model.add(layers.Conv2D(64,(7,7),2,padding="same"))
-# model.build(input_shape=(None,32,32,3))
-# model.compile(optimizer="adam",loss="categorical_crossentropy")
-# model.summary()
-# model.fit(x_train,y_train,epochs=10,batch_size=100)
-# y_pre = model.predict(x_test)
-# print(y_pre)
-# print(y_pre.shape())
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+x_train = x_train / 255
+x_test = x_test / 255
+# model construction
 model = ResNet18(10)
-model.build(input_shape=(None,32,32,3))
-model.compile(optimizer="adam",loss="categorical_crossentropy")
+model.build(input_shape=(None, 32, 32, 3))
+model.compile(optimizer="adam",
+              loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
 model.summary()
+
+# train
+model.fit(x_train, y_train, epochs=10, batch_size=32, validation_split=0.2)
+
+y_pre = model.predict(x_test)
+print(y_pre)
+y_pre_label = np.argmax(y_pre, axis=1)
+print(y_pre_label)
+print(y_test)
